afb17bccc571f092cb3d82a0aca081d94dd11966
Billion101 <billionbs20@gmail.com>
2023-05-20 14:56:23 +0700

    addnote
    yghyhy
    
    Co-Authored-By: Mr.Phonpasit Bounkham <100587437+pbig002@users.noreply.github.com>

--- lib/pages/add_note.dart
+++ lib/pages/add_note.dart
@@ -1,10 +1,146 @@
 import 'package:flutter/material.dart';
 
-class NotePage extends StatelessWidget {
-  const NotePage({super.key});
+
+class AddNote extends StatefulWidget {
+  const AddNote({Key? key}) : super(key: key);
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _AddNoteState createState() => _AddNoteState();
+}
+
+class _AddNoteState extends State<AddNote> {
+  get products => null;
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    // ignore: prefer_typing_uninitialized_variables
+    var products;
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        iconTheme: const IconThemeData(color: Colors.black),
+        actionsIconTheme: const IconThemeData(color: Colors.black),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(
+              Icons.push_pin_outlined,
+            ),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(
+              Icons.notifications_outlined,
+            ),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(
+              Icons.dashboard_outlined,
+            ),
+          ),
+        ],
+      ),
+      bottomNavigationBar: Container(
+        height: 100,
+        decoration: BoxDecoration(color: Colors.white, boxShadow: [
+          BoxShadow(
+            color: Colors.blue.withOpacity(0.5),
+            spreadRadius: 2.0,
+            blurRadius: 8.0,
+          )
+        ]),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Column(
+              children: [
+                Spacer(),
+                InkWell(
+                  onTap: () {},
+                  child: Container(
+                    decoration: BoxDecoration(
+                        color: Colors.white,
+                        shape: BoxShape.circle,
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.blue.withOpacity(0.5),
+                            spreadRadius: 2.0,
+                            blurRadius: 8.0,
+                          )
+                        ]),
+                    padding: const EdgeInsets.all(10.0),
+                    child: const Icon(
+                      Icons.check,
+                    ),
+                  ),
+                ),
+                Spacer(),
+                Row(
+                  children: List.generate(
+                      products.length, (index) => colorSelection(index)),
+                ),
+                Spacer(),
+              ],
+            ),
+          ],
+        ),
+      ),
+      body: SafeArea(
+        child: Column(
+          children: [
+            //title
+            TextFormField(
+              style: const TextStyle(
+                fontWeight: FontWeight.w600,
+                fontSize: 20,
+              ),
+              decoration: const InputDecoration(
+                hintText: "Enter title",
+                enabledBorder: OutlineInputBorder(
+                  borderSide: BorderSide.none,
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide.none,
+                ),
+              ),
+            ),
+
+            TextFormField(
+              style: const TextStyle(fontSize: 16, color: Colors.black),
+              decoration: const InputDecoration(
+                hintText: "Enter description",
+                enabledBorder: OutlineInputBorder(
+                  borderSide: BorderSide.none,
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide.none,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Padding colorSelection(int index) {
+    return Padding(
+      padding: const EdgeInsets.only(left: 10.0),
+      child: InkWell(
+        onTap: () {},
+        child: Container(
+          height: 30,
+          width: 30,
+          decoration: BoxDecoration(
+              color: products[index].color,
+              borderRadius: BorderRadius.circular(10.0)),
+        ),
+      ),
+    );
   }
 }
+
